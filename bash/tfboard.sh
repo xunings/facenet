5efea5b9d60615dfb5da9fc8d296c2a6c1ac1ebb
xunings <arc.neco@gmail.com>
2019-04-26 15:39:26 +0800

    Configurable port for the tfboard script.

--- bash/tfboard.sh
+++ bash/tfboard.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo Example usage: TBPORT=8083 ./tfboard.sh
 source /data/shaoxuning/.env/venv_tf1.7gpu_py3/bin/activate
 export CUDA_VISIBLE_DEVICES=6
-tensorboard --logdir=train_logs --port 8083 
+tensorboard --logdir=train_logs --port $TBPORT 
