9a734575a0d7dfc18822b783a734cbfc2dbea8d4
xunings <arc.neco@gmail.com>
2019-04-28 10:02:11 +0800

    Use cpu for tf f board.

--- bash/tfboard.sh
+++ bash/tfboard.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo Example usage: TBPORT=8083 ./tfboard.sh
 source /data/shaoxuning/.env/venv_tf1.7gpu_py3/bin/activate
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=-1
 tensorboard --logdir=train_logs --port $TBPORT 
