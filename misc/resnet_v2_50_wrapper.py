507595827c6e9e566df7b801f7199ff2e6c926e4
shaoxuning <shaoxuning@corp.netease.com>
2019-05-16 17:28:41 +0800

    The updates_collections config of batchnorm seemed to be failing. Try to fix it by setting arg_scope manually instead of calling resnet_utils.resnet_arg_scope.

--- misc/resnet_v2_50_wrapper.py
+++ misc/resnet_v2_50_wrapper.py
@@ -32,38 +32,50 @@ import tensorflow.contrib.slim as slim
 
 def inference(images, keep_probability=None, phase_train=True,
               bottleneck_layer_size=512, weight_decay=0.0, reuse=None):
-    # There is no dropout in resnet_v2,
+
+    with slim.arg_scope(
+            resnet_arg_scope(
+                is_training=phase_train,
+                weight_decay=weight_decay,
+                batch_norm_decay=0.997,
+                batch_norm_epsilon=1e-5)):
+        net, endpoints = resnet_v2.resnet_v2_50(images, is_training=phase_train, reuse=reuse)
+        net = tf.squeeze(net, axis=[1, 2])
+        net = slim.dropout(net, keep_probability, is_training=phase_train,
+                           scope='Dropout')
+        endpoints['PreLogitsFlatten'] = net
+        net = slim.fully_connected(net,
+                                   bottleneck_layer_size,
+                                   weights_initializer=slim.initializers.xavier_initializer(),
+                                   activation_fn=None,
+                                   scope='Bottleneck',
+                                   reuse=False)
+
+    return net, endpoints
+
+def resnet_arg_scope(is_training=True,
+                     weight_decay=0.0001,
+                     batch_norm_decay=0.997,
+                     batch_norm_epsilon=1e-5,
+                     batch_norm_scale=True):
     batch_norm_params = {
+        'is_training': is_training,
         # Decay for the moving averages.
-        'decay': 0.997,
+        'decay': batch_norm_decay,
         # epsilon to prevent 0s in variance.
-        'epsilon': 1e-5,
+        'epsilon': batch_norm_epsilon,
+        'scale': batch_norm_scale,
         # force in-place updates of mean and variance estimates
         'updates_collections': None,
         # Moving averages ends up in the trainable variables collection
         'variables_collections': [tf.GraphKeys.TRAINABLE_VARIABLES],
     }
 
-    with slim.arg_scope(
-            resnet_v2.resnet_arg_scope(
-                weight_decay=weight_decay,
-                batch_norm_decay=0.997,
-                batch_norm_epsilon=1e-5)):
-        # force in-place updates of mean and variance estimates
-        with slim.arg_scope([slim.batch_norm], updates_collections=None):
-            net, endpoints = resnet_v2.resnet_v2_50(images, is_training=phase_train, reuse=reuse)
-            net = tf.squeeze(net, axis=[1,2])
-            net = slim.dropout(net, keep_probability, is_training=phase_train,
-                               scope='Dropout')
-            endpoints['PreLogitsFlatten'] = net
-            net = slim.fully_connected(net,
-                                       bottleneck_layer_size,
-                                       weights_initializer=slim.initializers.xavier_initializer(),
-                                       weights_regularizer=slim.l2_regularizer(weight_decay),
-                                       normalizer_fn=slim.batch_norm,
-                                       normalizer_params=batch_norm_params,
-                                       activation_fn=None,
-                                       scope='Bottleneck',
-                                       reuse=False)
-
-    return net, endpoints
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        weights_regularizer=slim.l2_regularizer(weight_decay),
+                        weights_initializer=slim.variance_scaling_initializer(),
+                        normalizer_fn=slim.batch_norm,
+                        normalizer_params=batch_norm_params):
+        with slim.arg_scope([slim.batch_norm], **batch_norm_params):
+            with slim.arg_scope([slim.max_pool2d], padding='SAME') as arg_sc:
+                return arg_sc
